5417c902364189da8aafddec9be048d5bd78cab0
Juan Jose Rendon <juanjoserendon13@outlook.com>
2019-10-09 14:59:14 -0500

    custom useState Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import useForm from './hooks/useForm';
 
 function App() {
+  const [values, handleChange] = useForm({ email: "", password: "" });
   return (
     <div className="App">
-      Content
+      <input
+        type="text"
+        name="email"
+        value={values.email}
+        onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange} />
     </div>
   );
 }
