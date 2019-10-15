78303a4d3363dbc021544120df5b221086e01852
Juan Jose Rendon <juanjoserendon13@outlook.com>
2019-10-15 16:46:31 -0500

    useMemo implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useMemo } from 'react';
 import Child from './components/Child';
 import Item from './components/Item';
 
@@ -22,14 +22,34 @@ function App() {
   const inputRef = useRef();
   const [showChild, setShowChild] = useState(true);
   const [count, setCount] = useState(0);
+  const { data } = useFetch('https://api.kanye.rest/ajzbc/kanye.rest/master/quotes.json')
   const items = [10, 20, 30];
 
+  // Use callback to pass returned callback as a prop to child components
+  // (avoiding function recreation) to use in events like onClick, onChange, etc.
   const increment = useCallback(
     n => {
       setCount(c => c + n);
     },
     [setCount]
   );
+
+  const computeLongestWord = (sentence) => {
+    console.log('computed');
+    if (!sentence) {
+      return [];
+    }
+    let longestWord = '';
+    JSON.parse(sentence).quote.split(' ').forEach(w => {
+      if (w.length > longestWord.length) {
+        longestWord = w;
+      }
+    });
+    return longestWord;
+  };
+
+  // Use memo for expensive calculations in the current component
+  const longestWord = useMemo(() => computeLongestWord(data), [data]);
 
   return (
     <div className="App">
@@ -53,6 +73,7 @@ function App() {
       {items.map(n => {
         return <Item increment={increment} n={n} key={n} />;
       })}
+      <div>Computed sentence: {longestWord}</div>
     </div>
   );
 }
